4bdf21a4c2a5c75010ec5ff3db0efb2867b079fc
Siri Pantzar <siri.juulia@gmail.com>
2024-12-19 20:56:36 +0000

    add door open close feature to backdoor

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const doorOne = document.querySelector(".doorOne")
+doorOne.addEventListener("click", toggleDoorOne);
+
+function toggleDoorOne(ev) {
+  doorOne.classList.toggle("doorOpen");
+  ev.stopPropagation();
+}
+
+const bdOne = document.querySelector(".bdOne");
+bdOne.addEventListener("click", toggleDoorOne);
+
+const doorTwo = document.querySelector(".doorTwo")
+doorTwo.addEventListener("click", toggleDoorTwo);
+
+function toggleDoorTwo(ev) {
+  doorTwo.classList.toggle("doorOpen");
+  ev.stopPropagation();
+}
+
+const bdTwo = document.querySelector(".bdTwo")
+bdTwo.addEventListener("click", toggleDoorTwo);
+
+const doorThree = document.querySelector(".doorThree")
+doorThree.addEventListener("click", toggleDoorThree);
+
+function toggleDoorThree(ev) {
+  doorThree.classList.toggle("doorOpen");
+  ev.stopPropagation();
+}
+
+const bdThree = document.querySelector(".bdThree")
+bdThree.addEventListener("click", toggleDoorThree);
+
+const doorFour = document.querySelector(".doorFour")
+doorFour.addEventListener("click", toggleDoorFour);
+
+function toggleDoorFour(ev) {
+  doorFour.classList.toggle("doorOpen");
+  ev.stopPropagation();
+}
+
+const bdFour = document.querySelector(".bdFour")
+bdFour.addEventListener("click", toggleDoorFour);
+
 /*const 
 doors = document.querySelectorAll(".door"); //array of doors
 window.selected = doors;
@@ -10,31 +54,3 @@ function toggleDoor(element) {
     element.innerHTML = "<p>You've selected this door!<p>";
   }
 */
-
-const doorOne = document.querySelector(".doorOne")
-doorOne.addEventListener("click", toggleDoorOne);
-
-function toggleDoorOne() {
-  doorOne.classList.toggle("doorOpen");
-}
-
-const doorTwo = document.querySelector(".doorTwo")
-doorTwo.addEventListener("click", toggleDoorTwo);
-
-function toggleDoorTwo() {
-  doorTwo.classList.toggle("doorOpen");
-}
-
-const doorThree = document.querySelector(".doorThree")
-doorThree.addEventListener("click", toggleDoorThree);
-
-function toggleDoorThree() {
-  doorThree.classList.toggle("doorOpen");
-}
-
-const doorFour = document.querySelector(".doorFour")
-doorFour.addEventListener("click", toggleDoorFour);
-
-function toggleDoorFour() {
-  doorFour.classList.toggle("doorOpen");
-}
